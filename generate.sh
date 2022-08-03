a7386553e68e854a66d590a7f210ae372d6bbfae
Tim Cooijmans <timcooijmans@gmail.com>
2022-08-03 14:29:00 +0200

    first commit

---
+++ generate.sh
@@ -0,0 +1,7 @@
+protoc --go_out=./go \
+  --go_opt=paths=source_relative \
+  --go-grpc_out=./go \
+  --go-grpc_opt=paths=source_relative \
+  -I ./chirpstack-api/protobuf/ \
+  -I ./protobuf \
+  router/router-api.proto
